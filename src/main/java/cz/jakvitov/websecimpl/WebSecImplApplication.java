f76ca10941ff2ffc60ec59968cfb9e981104805a
Jakub Vítovec <jakvitov@gmail.com>
2023-04-23 10:18:07 +0200

    Initial commit with project setup

---
+++ src/main/java/cz/jakvitov/websecimpl/WebSecImplApplication.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.websecimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/jakvitov/websecimpl/WebSecImplApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.websecimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
